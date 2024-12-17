76bf09d9927da4e2f5d9a367efebfa53119834cd
Choi Daniel <daniell8282@naver.com>
2024-12-17 14:26:07 +0900

    FBV로 블로그 포스트 목록 페이지 만들기

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
 
     # auto_now_add=True 를 해줘야 추후에 글 생성시, 시간 등을 선택하지 않고 자동반영 될 수 있음
     created_at = models.DateTimeField(auto_now_add=True)
-    upadated_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         # 포스트의 pk 값 , 포스트의 title 값
