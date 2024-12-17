ab26cb6327db6990db85f81f0a5cd99c16651328
Choi Daniel <daniell8282@naver.com>
2024-12-17 13:31:25 +0900

    Post 모델을 생성하고 시간 자동반영

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+
+    # auto_now_add=True 를 해줘야 추후에 글 생성시, 시간 등을 선택하지 않고 자동반영 될 수 있음
+    created_at = models.DateTimeField(auto_now_add=True)
+    upadated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        # 포스트의 pk 값 , 포스트의 title 값
+        return f"[{self.pk}] {self.title}"
