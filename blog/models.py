64d939f9e5f30cc548c40c11d4b8ef19dc29cd4d
Choi Daniel <daniell8282@naver.com>
2024-12-18 13:31:11 +0900

    Post 모델에 file_upload 필드 추가

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,8 @@ class Post(models.Model):
 
     # blank=True는 필수 입력값이 아니라는 뜻
     head_image = models.ImageField(upload_to="blog/images/%Y/%m/%d/", blank=True)
+    file_upload = models.FileField(upload_to="blog/files/%Y/%m/%d/", blank=True)
+
     # auto_now_add=True 를 해줘야 추후에 글 생성시, 시간 등을 선택하지 않고 자동반영 될 수 있음
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
