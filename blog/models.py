42474caa2c0618d23d538a60dffdd67550e3b542
Choi Daniel <daniell8282@naver.com>
2024-12-18 16:51:45 +0900

    318~323 test

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,14 @@ from django.contrib.auth.models import User
 import os
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
     title = models.CharField(max_length=30)
     hook_text = models.CharField(max_length=100, blank=True)
@@ -23,10 +31,16 @@ class Post(models.Model):
     # SET_NULL은 해당 작성자가 데이터베이스에서 삭제되면, 그 작성자가 작성한 포스트 작성자 명은 공란이 된다.
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+
     # f-string 기법
     def __str__(self):
         # 포스트의 pk 값 , 포스트의 title 값
         return f"[{self.pk}] {self.title} :: {self.author}"
+
+    # admin 단에서 보여지는 Category 복수형의 이름을 수정해주는 것
+    class Meta:
+        verbose_name_plural = "Categories"
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
