a803c3073bf18b5b38cbe8e14f523f35dcea0169
Choi Daniel <daniell8282@naver.com>
2024-12-18 22:18:14 +0900

    포스트를 수정할 때 또한 태그 반영

--- blog/views.py
+++ blog/views.py
@@ -84,6 +84,36 @@ class PostUpdate(LoginRequiredMixin, UpdateView):
         else:
             raise PermissionDenied
 
+    def get_context_data(self, **kwargs):
+        context = super(PostUpdate, self).get_context_data()
+        if self.object.tags.exists():
+            tags_str_list = list()
+            for t in self.object.tags.all():
+                tags_str_list.append(t.name)
+            context['tags_str_default'] = '; '.join(tags_str_list)
+
+        return context
+
+    def form_valid(self, form):
+        response = super(PostUpdate, self).form_valid(form)
+        self.object.tags.clear()
+
+        tags_str = self.request.POST.get('tags_str')
+        if tags_str:
+            tags_str = tags_str.strip()
+            tags_str = tags_str.replace(',',';')
+            tags_list = tags_str.split(';')
+
+            for t in tags_list:
+                t = t.strip()
+                tag, is_tag_craeted = Tag.objects.get_or_create(name=t)
+                if is_tag_craeted:
+                    tag.slug = slugify(t, allow_unicode=True)
+                    tag.save()
+                self.object.tags.add(tag)
+
+        return response
+
 # FBV 스타일
 def tag_page(request, slug):
     tag = Tag.objects.get(slug=slug)
