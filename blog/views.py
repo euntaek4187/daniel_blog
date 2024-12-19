32f5758b26ec23b95c5681b7311708d2de7ae38b
Choi Daniel <daniell8282@naver.com>
2024-12-19 21:01:03 +0900

    일반계정 새포스트 버튼 안보임 + ModelForm 포스트 디테일 에러 + 댓글폼으로 포스트 상세 페이지에서 댓글 작성

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,13 @@
 from PIL.ImageFilter import DETAIL
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import ListView, DeleteView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from .models import Post, Category, Tag
 from django.core.exceptions import PermissionDenied
 from django.utils.text import slugify
+
+
+from .models import Post, Category, Tag
+from .forms import CommentForm
 
 
 # Create your views here.
@@ -34,6 +37,7 @@ class PostDetail(DeleteView):
         context = super(PostDetail, self).get_context_data()
         context['categories'] = Category.objects.all()
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        context["comment_form"] = CommentForm
         return context
 
 
@@ -151,6 +155,23 @@ def category_page(request, slug):
             "category": category
         }
     )
+
+def new_comment(request, pk):
+    if request.user.is_authenticated:
+        post = get_object_or_404(Post, pk=pk)
+
+        if request.method == "POST":
+            comment_form = CommentForm(request.POST)
+            if comment_form.is_valid():
+                comment = comment_form.save(commit=False)
+                comment.post = post
+                comment.author = request.user
+                comment.save()
+                return redirect(comment.get_absolute_url())
+        else:
+            return redirect(post.get_absolute_url())
+    else:
+        raise PermissionDenied
 
 """
 def index(request):
