a7eea299e4ddb422f86aa136daf5bac044f8f2d4
최다니엘 <daniell8282@naver.com>
2024-12-30 15:29:08 +0900

    settings allowed host change

--- daniel_blog/settings.py
+++ daniel_blog/settings.py
@@ -29,12 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY","django-insecure-88ndzs0z0uzko4x5f=*ngu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "1").lower() in ("true", "1", "yes")
 
-if os.environ.get("DJANGO_ALLOWED_HOSTS"):
-    ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(' ')
-else:
-    ALLOWED_HOSTS = []
-
-ALLOWED_HOSTS = ['danielblogbla.netlify.app', 'daniel4191.com']
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", '').split() if os.environ.get("DJANGO_ALLOWED_HOSTS") else ['danielblogbla.netlify.app', 'daniel4191.com', "localhost", "127.0.0.1"]
 
 # Application definition
 
