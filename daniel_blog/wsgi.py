63e8e44d16c23168c441ea143c98683e331eea35
최다니엘 <daniell8282@naver.com>
2025-01-04 20:47:55 +0900

    give up publish

--- daniel_blog/wsgi.py
+++ daniel_blog/wsgi.py
@@ -1,16 +1,23 @@
-"""
-WSGI config for daniel_blog project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
+# wsgi.py
 
 import os
+import sys
+
 from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "daniel_blog.settings")
-application = get_wsgi_application()
+# 가상환경 경로 설정 (로컬 환경에서 설정하는 부분)
+# activate_this = "/path/to/your/virtualenv/bin/activate_this.py"
+# with open(activate_this) as file_:
+#     exec(file_.read(), dict(__file__=activate_this))
 
-app = application
+# 프로젝트 경로 설정 (로컬 환경에서 설정하는 부분)
+# project_root = "/path/to/your/project"
+# if project_root not in sys.path:
+#     sys.path.append(project_root)
+
+# Django 설정 모듈 설정
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "your_project_name.settings")
+
+# WSGI 애플리케이션 반환
+application = StaticFilesHandler(get_wsgi_application())
